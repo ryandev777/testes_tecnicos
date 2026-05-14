1f09098189b5ed3e0800fd1a9a68569750384302
Ryan <joaoryanoliveira2005@gmail.com>
2026-05-14 11:29:24 -0300

    feat: desafio: buscar dados específicos de uma api utilizando Pick afim de manter a tipagem forte

--- src/service/desafio_1/desafio-1.ts
+++ src/service/desafio_1/desafio-1.ts
@@ -1,3 +1,5 @@
+import axios = require("axios");
+
 enum UserRole {
     DIRETOR = 'DIRETOR',
     GERENTE = 'GERENTE',
@@ -7,12 +9,47 @@ enum UserRole {
 
 
 
-interface user {
+interface User {
     id: number;
     name: string;
     email: string;
     password: string;
+    role: UserRole;
     isActive: boolean;
 }
 
+type UserSumary = Pick<User, 'id' | 'name' | 'email' | 'role'>
 
+
+async function getUsersMockData(): Promise <UserSumary[]> {
+    try {
+        const response = await axios.get<User[]>('https://6a05d15aaa826ca75c0aa767.mockapi.io/api/desafio1/users')
+        const users = response.data;
+
+        const activeManengers = users
+        .filter(user => user.isActive === true && user.role === UserRole.PEAO)
+        .map( user => {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role 
+             
+            }
+        })
+        return activeManengers;
+    } catch(err) {
+        console.error('erro ao buscar , falta algo ai', err)
+        return []
+    }
+}
+
+
+async function executeGet() {
+    console.log('buscando');
+    const manegers = await getUsersMockData()
+    console.log('exibindos os peões da empresa', manegers);
+    
+}
+
+executeGet()
